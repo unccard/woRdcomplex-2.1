5383f45872d457b2cf2d4966b7b5afb3447957da
lindsay-greene <lindsay.smile@yahoo.com>
2021-04-18 17:46:01 -0400

    switch to unc comb db

--- woRdcomplex-2.1.R
+++ woRdcomplex-2.1.R
@@ -22,7 +22,7 @@ engl_liquids <- c("l","L","r","R","X")
 
 phonetic<-tibble()
 stress<-tibble()
-mrc<-read.csv('mrc2.csv', na.strings=c("", "NA"))
+data<-read.csv('UNCCombWordDB.csv', na.strings=c("", "NA"))
 fileNames = dir(pattern = ".txt")
 for (fileName in fileNames){
   phonetic<-tibble()
@@ -30,33 +30,35 @@ for (fileName in fileNames){
   data<-{}
   sample <- readChar(fileName, file.info(fileName)$size)
   sample<-as.character(sample) # returns sample as text representation
-  sample<-str_to_upper(sample, locale="en") #converts to uppercase
+  sample<-str_to_lower(sample, locale="en") #converts to lowercase to match DB file 
   text_df<-tibble(text=sample)  #tibble is a simple data frame
   text_df <-text_df%>%
-    unnest_tokens(word, text) #this breaks the column into words, one token(word) per row
-  tibbletest <-tibble(mrc$word, mrc$phon) #looks like setting up a variable that includes only the word and phon columns from MRC
+  unnest_tokens(word, text) #this breaks the column into words, one token(word) per row
+  tibbletest <-tibble(data$word, data$phon_klattese) #looks like setting up a variable that includes only the word and phon columns from file
   tibbletest <- na.omit(tibbletest) #na.omit gets rid of cases where the value is na
-  concrete <-na.omit(tibble(mrc$word, mrc$conc)) # this creates a variable of concreteness which does not produce any output
+  #concrete <-na.omit(tibble(data$word, data$conc)) # this creates a variable of concreteness which does not produce any output
   
   tibbletest <- na.omit(tibbletest) # i don't understand how this is different from two lines above.
   for (i in 1:nrow(text_df)){
-    r<-which(toupper(text_df$word[i])==tibbletest$`mrc$word`, arr.ind = TRUE) # toupper goes to uppercase. why are we doing this?
-    # i guess that line defines r as which words have a value that can be found in the MRC file.
+    r<-which(toupper(text_df$word[i])==tibbletest$`data$word`, arr.ind = TRUE) # toupper goes to uppercase. why are we doing this?
+    # i guess that line defines r as which words have a value that can be found in the file.
     
-    phonetic <- paste(phonetic, tibbletest$`mrc$phon`[r[1]]) # not really sure what this does. from a test, it looks like it is one variable, which is the phonetic transcription of each word
+    phonetic <- paste(phonetic, tibbletest$`data$phon_klattese`[r[1]]) # not really sure what this does. from a test, it looks like it is one variable, which is the phonetic transcription of each word
     phonetic<-gsub("NA", "", phonetic) #substitute NA with blank
-    phonetic<-str_split(string=phonetic, pattern=" ") #i think this splits the string whenever there is a space
+    #phonetic<-str_split(string=phonetic, pattern=" ") #i think this splits the string whenever there is a space - also no longer need?
     phonetic<-as.data.frame(phonetic, stringsAsFactors=FALSE) #makes this into a dataframe
     
-    tibbletest$`mrc$phon`[30407]
+    tibbletest$`data$phon_klattese`[30407]
     points<-0  
     for (word in 1:nrow(phonetic)){
       
       # BEGIN new solution 
       
-      len<-str_length(word)  # number of characters in the word 
+      len <- str_length(word)  # number of characters in the word 
+      polysyll <- data$polysyll
+      nonInitPrimStress <- data$non-initialPrimaryStress 
       if (polysyll == 1) points=points+1  #word patterns (1)
-      if (nonInitialPrimaryStress == 1) points=points+1  #word patterns (2)
+      if (nonInitPrimStress == 1) points=points+1  #word patterns (2)
      
       # for loop to find consonant clusters and sound classes 
       for (index in 1:len) {
