ba49d2ac16f7c4560fdc9f390a1fb96c3f8676a8
lindsay-greene <lindsay.smile@yahoo.com>
2021-09-14 13:17:58 -0400

    reset to 'word by word readable in excel'

--- woRdcomplex-2.1.R
+++ woRdcomplex-2.1.R
@@ -4,12 +4,10 @@
 # This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation. AMDG. 
 # Script calculates the edit distance ratio for intelligible words in a sample. 
 # Requires CSV file "UNCCombWordDB.csv" and that the user specify a file path on line 27.  
-
 library(tidyr)
 library(tidytext)
 library(stringr)
 library(dplyr)
-library("xlsx")
 
 # phoneme categories 
 engl_voiceless_cons <- c("C","f","h","k","p","s","S","t","T")
@@ -19,28 +17,23 @@ engl_fricatives <- c("D","f","h","s","S","T","v","z","Z")
 engl_affricates <- c("C","J")
 engl_velars <- c("k","g","G")
 engl_liquids <- c("l","L","r","R","X")
-
 word_db <- read.csv('UNCCombWordDB.csv', na.strings=c("", "NA"))
-
 # TO DO: fill in arguments of data.path with path to directory containing .txt files, leaving first argument blank 
 # for example: /Users/folder1/folder2 -> data_path("", "Users", "folder1", "folder2")
 data_path <- file.path("", "Users", "lindsaygreene", "Desktop")
-
 # set up data frame to store results 
 data <- data.frame(matrix(vector(), ncol=7, nrow=length(files)))  # data frame to store avg output  
 files <- list.files(path=data_path, pattern="*.txt")
 header_names <- list("Total_Words_in_Tscript", "Total_Words_Found_in_DB","Avg_Phon_Score", 
-                    "Avg_WF_Score", "Avg_Fam_Score", "Avg_Conc_Score", "Avg_Imag_Score")  # column headers for avg output df 
+                     "Avg_WF_Score", "Avg_Fam_Score", "Avg_Conc_Score", "Avg_Imag_Score")  # column headers for avg output df 
 colnames(data) <- header_names
 rownames(data) <- files
-
 # set up word by word analysis 
 word_by_word <- data.frame(matrix(vector(), ncol=5))  # data frame to store info ab individual words from each transcript
 names <- list("File_Name", "Word", "Phonetic_Word", "WCM_Score", "Word_Frequency")  # column headers for word by word df 
 colnames(word_by_word) <- names
 wbw_row = 1  # count num rows in word by word db 
-
-for (file in 1:length(files)){
+for (file in 1:length(files)) {
   
   fileName <- files[file]
   filePath <- paste(data_path, "/", fileName, sep="")  # update file name to absolute path 
@@ -51,7 +44,7 @@ for (file in 1:length(files)){
   sample<-str_to_lower(sample, locale="en")  # convert sample to lowercase to match DB file 
   text_df<-tibble(text=sample)  # convert sample to tibble (a simple data frame) 
   text_df <-text_df%>%  # way of filtering the data 
-  unnest_tokens(word, text)  # break the column into one word per row 
+    unnest_tokens(word, text)  # break the column into one word per row 
   tibbletest <-tibble(word_db$word, word_db$phon_klattese, word_db$polysyll, word_db$nonInitialPrimaryStress, 
                       word_db$SUBTLWF0to10, word_db$fam, word_db$conc, word_db$imag)  # isolates the categories we need from word_db 
   
@@ -128,85 +121,86 @@ for (file in 1:length(files)){
     
     if (polysyll == 1) phon_points=phon_points+1  # word patterns (1)
     if (nonInitPrimStress == 1) phon_points=phon_points+1  # word patterns (2)
-     
+    
     # for loop to find consonant clusters and sound classes 
     for (index in 1:len) {
       phoneme <- substr(klattese, index, index)
       
       # isolate phonemes and remove stress marker 
       # this makes klattese more readable in csv file format 
-      if((phoneme >= 41 && phoneme >= 90) || (phoneme >= 61 && phoneme >= 122)) {
-        klattese_without_tilde = paste(klattese_without_tilde, phoneme, sep = "")
-      } else if(phoneme == '@' || phoneme == '^' || phoneme == '|') {
-        klattese_without_tilde = paste(klattese_without_tilde, phoneme, sep = "")
+        if((phoneme >= 41 && phoneme >= 90) || (phoneme >= 61 && phoneme >= 122)) {
+          klattese_without_tilde = paste(klattese_without_tilde, phoneme, sep = "")
+        } else if(charToRaw(phoneme) == 40 || charToRaw(phoneme) == 94 || charToRaw(phoneme) == 124) {
+        } else if(phoneme == '@' || phoneme == '^' || phoneme == '|') {
+          klattese_without_tilde = paste(klattese_without_tilde, phoneme, sep = "")
+        }
+        
+        if (index == len) {
+          if (phoneme %in% engl_voiced_cons | phoneme %in% engl_voiceless_cons | phoneme %in% engl_syll_cons) { 
+            phon_points=phon_points+1  # syllable structures (1)
+          } 
+        }
+        if (phoneme %in% engl_voiced_cons | phoneme %in% engl_voiceless_cons) {
+          i <- index
+          is_cluster <- FALSE 
+          while (i < len) {
+            next_phon <- substr(klattese, i+1, i+1)
+            if (next_phon %in% engl_voiced_cons | next_phon %in% engl_voiceless_cons) {
+              i=i+1
+              is_cluster <- TRUE
+            } 
+            else break
+          }
+          if (is_cluster) phon_points=phon_points+1  # syllable structures (2)
+        }
+        if (phoneme %in% engl_velars) phon_points=phon_points+1  # sound classes (1)
+        if (phoneme %in% engl_liquids) phon_points=phon_points+1  # sound classes (2)
+        if (phoneme %in% engl_fricatives | phoneme %in% engl_affricates) {
+          phon_points=phon_points+1  # sound classes (3)
+          if (phoneme %in% engl_voiced_cons) {
+            phon_points=phon_points+1  # sound classes (4)
+          }
+        }
       }
       
-      if (index == len) {
-        if (phoneme %in% engl_voiced_cons | phoneme %in% engl_voiceless_cons | phoneme %in% engl_syll_cons) { 
-          phon_points=phon_points+1  # syllable structures (1)
-        } 
-      }
-      if (phoneme %in% engl_voiced_cons | phoneme %in% engl_voiceless_cons) {
-        i <- index
-        is_cluster <- FALSE 
-        while (i < len) {
-          next_phon <- substr(klattese, i+1, i+1)
-          if (next_phon %in% engl_voiced_cons | next_phon %in% engl_voiceless_cons) {
-            i=i+1
-            is_cluster <- TRUE
-          } 
-          else break
-        }
-        if (is_cluster) phon_points=phon_points+1  # syllable structures (2)
-      }
-      if (phoneme %in% engl_velars) phon_points=phon_points+1  # sound classes (1)
-      if (phoneme %in% engl_liquids) phon_points=phon_points+1  # sound classes (2)
-      if (phoneme %in% engl_fricatives | phoneme %in% engl_affricates) {
-        phon_points=phon_points+1  # sound classes (3)
-        if (phoneme %in% engl_voiced_cons) {
-          phon_points=phon_points+1  # sound classes (4)
-        }
-      }
+      # END algorithm to calculate points for the word 
+      
+      # store info in word by word output 
+      word_by_word[wbw_row, 1] = fileName
+      word_by_word[wbw_row, 2] = foundInDB_tscript[word, 1]
+      word_by_word[wbw_row, 3] = klattese_without_tilde
+      word_by_word[wbw_row, 4] = phon_points
+      word_by_word[wbw_row, 5] = wf
+      
+      wbw_row = wbw_row + 1  # move to next row in database
+      
+      # adding points for current word to cumulative total 
+      phon_total = phon_total + phon_points 
+      wf_total = wf_total + wf  
     }
     
-    # END algorithm to calculate points for the word 
+    # loop through fam, conc, imag and add up non-zero values 
+    # for(i in 1:nrow(fam_tscript)) if(as.integer(fam_tscript[i,1])>0) fam_total = fam_total + as.integer(fam_tscript[i,1])
+    # for(i in 1:nrow(conc_tscript)) if(as.integer(conc_tscript[i,1])>0) conc_total = conc_total + as.integer(conc_tscript[i,1])
+    # for(i in 1:nrow(imag_tscript)) if(as.integer(imag_tscript[i,1])>0) imag_total = imag_total + as.integer(imag_tscript[i,1])
+  
+    # calculate averages for file from total points 
+    avg_phon <- phon_total/nrow(phonetic_tscript)
+    avg_wf <- wf_total/nrow(wf_tscript)
+    # avg_fam <- fam_total/(nrow(fam_tscript)-fam_null) 
+    # avg_conc <- conc_total/(nrow(conc_tscript)-conc_null)
+    # avg_imag <- imag_total/(nrow(imag_tscript)-imag_null) 
     
-    # store info in word by word output 
-    word_by_word[wbw_row, 1] = fileName
-    word_by_word[wbw_row, 2] = foundInDB_tscript[word, 1]
-    word_by_word[wbw_row, 3] = klattese_without_tilde
-    word_by_word[wbw_row, 4] = phon_points
-    word_by_word[wbw_row, 5] = wf
-    
-    wbw_row = wbw_row + 1  # move to next row in database
-    
-    # adding points for current word to cumulative total 
-    phon_total = phon_total + phon_points 
-    wf_total = wf_total + wf  
+    # write output and file name to avg output data frame  
+    data[file,1] = nrow(text_df)
+    data[file,2] = nrow(phonetic_tscript)
+    data[file,3] = avg_phon
+    data[file,4] = avg_wf 
+    # data[file,5] = avg_fam
+    # data[file,6] = avg_conc
+    # data[file,7] = avg_imag
   }
   
-  # loop through fam, conc, imag and add up non-zero values 
-  # for(i in 1:nrow(fam_tscript)) if(as.integer(fam_tscript[i,1])>0) fam_total = fam_total + as.integer(fam_tscript[i,1])
-  # for(i in 1:nrow(conc_tscript)) if(as.integer(conc_tscript[i,1])>0) conc_total = conc_total + as.integer(conc_tscript[i,1])
-  # for(i in 1:nrow(imag_tscript)) if(as.integer(imag_tscript[i,1])>0) imag_total = imag_total + as.integer(imag_tscript[i,1])
-
-  # calculate averages for file from total points 
-  avg_phon <- phon_total/nrow(phonetic_tscript)
-  avg_wf <- wf_total/nrow(wf_tscript)
-  # avg_fam <- fam_total/(nrow(fam_tscript)-fam_null) 
-  # avg_conc <- conc_total/(nrow(conc_tscript)-conc_null)
-  # avg_imag <- imag_total/(nrow(imag_tscript)-imag_null) 
-  
-  # write output and file name to avg output data frame  
-  data[file,1] = nrow(text_df)
-  data[file,2] = nrow(phonetic_tscript)
-  data[file,3] = avg_phon
-  data[file,4] = avg_wf 
-  # data[file,5] = avg_fam
-  # data[file,6] = avg_conc
-  # data[file,7] = avg_imag
-}
-
-# write output to file and save to same location to be opened in excel 
-write.csv(data, file=paste(data_path, "/", "wcm_output.csv", sep=""))
-write.csv(word_by_word, file=paste(data_path, "/", "word_by_word.csv", sep=""))
+  # write output to file and save to same location to be opened in excel 
+  write.csv(data, file=paste(data_path, "/", "wcm_output.csv", sep=""))
+  write.csv(word_by_word, file=paste(data_path, "/", "word_by_word.csv", sep=""))
