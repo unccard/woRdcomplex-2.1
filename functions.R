62734da00b4c38b7ef42f02e68c2f992c4019fec
lindsay-greene <lindsay.smile@yahoo.com>
2021-10-29 17:05:42 -0400

    polysyll and syllabic cons

--- functions.R
+++ functions.R
@@ -2,7 +2,7 @@ calculateWCM<- function(klattese) {  # calculate WCM score for the word
   # phoneme categories 
   engl_voiceless_cons <- c("C","f","h","k","p","s","S","t","T")
   engl_voiced_cons <- c("b","d","D","F","g","J","l","M","m","N","n","G","r","v","w","y","z","Z")  # word final M and N? 
-  # engl_syll_cons <- c("L", "M", "N", "R") 
+  engl_syll_cons <- c("L", "M", "N", "R") 
   engl_fricatives <- c("D","f","h","s","S","T","v","z","Z")
   engl_affricates <- c("C","J")
   engl_velars <- c("k","g","G")
@@ -15,12 +15,12 @@ calculateWCM<- function(klattese) {  # calculate WCM score for the word
   # if the word ends in a consonant 
   len <- str_length(klattese)
   final_phoneme <- substr(klattese, len, len)
-  if (final_phoneme %in% engl_voiced_cons | final_phoneme %in% engl_voiceless_cons) { 
+  if (final_phoneme %in% engl_voiced_cons | final_phoneme %in% engl_voiceless_cons | final_phoneme %in% engl_syll_cons) { 
     phon_points=phon_points+1  # syllable structures (1)
   } 
   
   # if the word has consonant clusters 
-  split <- strsplit(klattese, "([iIEe@aWY^cOoUuRx|X\\ˈ]+|-+)+")  # regular expression to isolate consonants 
+  split <- strsplit(klattese, "([iIEe@aWY^cOoUuRx|XLMNR\\ˈ]+|-+)+")  # regular expression to isolate consonants 
   for(i in 1:length(split[[1]])) {
     if(str_length(split[[1]][i]) > 1) { 
       phon_points = phon_points + 1  # syllable structures (2)
@@ -43,7 +43,7 @@ calculateWCM<- function(klattese) {  # calculate WCM score for the word
     }
   }
   # WCM rules for word patterns 
-  if (syllables > 2) phon_points=phon_points+1  # word patterns (1)
+  if (syllables >= 2) phon_points=phon_points+1  # word patterns (1)
   if (nonInitPrimStress == 1) phon_points=phon_points+1  # word patterns (2)
   
   return(phon_points) 
