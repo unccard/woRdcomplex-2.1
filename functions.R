b82b0dcb2baf6838379990727d9fdc7d5424a0c4
Adam Jacks <68718748+jaxtastic@users.noreply.github.com>
2021-12-02 12:51:08 -0500

    correct multisyll points
    line 82 changed >= to >; words with more than 2 syllables (3 or more) should get a point, but not words with only 2 syllables.

--- functions.R
+++ functions.R
@@ -79,7 +79,7 @@ calculateWCM <- function(klattese) {  # calculate WCM score for the word
     }
   }
   # WCM rules for word patterns 
-  if (syllables >= 2) phon_points=phon_points+1  # word patterns (1)
+  if (syllables > 2) phon_points=phon_points+1  # word patterns (1)
   if (nonInitPrimStress == 1) phon_points=phon_points+1  # word patterns (2)
   
   return(phon_points) 
